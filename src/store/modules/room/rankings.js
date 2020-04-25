04b9f5fe5cdf9419fbd4f1e1a92ec6cfc306439a
KasuraS <56578235+KasuraS@users.noreply.github.com>
2020-04-25 10:36:51 +0200

    rankings store update

--- src/store/modules/room/rankings.js
+++ src/store/modules/room/rankings.js
@@ -1,25 +1,52 @@
 const state = {
-  rankedPlayers: []
+  playerScoresList: []
 }
 
 const getters = {
-  getRankedPlayers: state => state.rankedPlayers,
-  getRankedPlayersByUser: state => username => {
-    return state.rankedPlayers.find(_ => _.username === username)
-  }
+  getPlayerScoresList: state => state.playerScoresList.sort((a, b) => (a.points <= b.points && a.username > b.username) ? 1 : -1)
 }
 
 const mutations = {
-  update (state, { data }) {
-    state.rankedPlayers = [].concat(data)
+  addJoinedPlayer (state, data) {
+    state.playerScoresList.push({ username: data.playerUsername, points: 0 })
+    state.playerScoresList.sort((a, b) => (a.points <= b.points && a.username > b.username) ? 1 : -1)
+  },
+  removeLeftPlayer (state, data) {
+    state.playerScoresList.splice(state.playerScoresList.findIndex(
+      function (player, index) {
+        if (player.username === data.playerUsername) {
+          return index
+        }
+      }), 1)
+  },
+  clearPlayerScoresList (state) {
+    state.playerScoresList = []
+  },
+  updatePlayerScoresList (state, data) {
+    state.playerScoresList = data.players
+    state.playerScoresList.sort((a, b) => (a.points <= b.points && a.username > b.username) ? 1 : -1)
+  },
+  updatePlayerScoresList2 (state, data) {
+    for (let i = 0; i < state.playerScoresList.length; i++) {
+      if (state.playerScoresList[i].username === data.pointUpdate.playerUsername) {
+        state.playerScoresList[i].points = data.pointUpdate.points
+      }
+    }
   }
 }
 
 const actions = {
-  async SOCKET_rankedPlayersByRoom (state, data) {
-    state.commit('update', {
-      data: data.players
-    })
+  async SOCKET_addJoinedPlayer (state, data) {
+    state.commit('addJoinedPlayer', data)
+  },
+  async SOCKET_removeLeftPlayer (state, data) {
+    state.commit('removeLeftPlayer', data)
+  },
+  async SOCKET_updatePlayerScoresList (state, data) {
+    state.commit('updatePlayerScoresList', data)
+  },
+  async SOCKET_updatePlayerScoresList2 (state, data) {
+    state.commit('updatePlayerScoresList2', data)
   }
 }
 
